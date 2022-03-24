79d38bb3bf52704339bfa7f59013c1ef467f2d77
Lize <lizeduplessis.dev@gmail.com>
2022-03-24 11:31:57 +0000

    showmore button

--- pages/countries/[id].js
+++ pages/countries/[id].js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 export const getStaticPaths = async () => {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
@@ -27,12 +28,26 @@ https://restcountries.com/v3.1/name/${id}`);
 };
 
 const Details = ({ country }) => {
+  const [showMoreBtn, setShowMoreBtn] = useState(false);
+  const [inputText, setInputText] = useState("");
+  const [submitText, setSubmitText] = useState("");
+
+  const handleShowMore = (e) => {
+    setShowMoreBtn(!showMoreBtn);
+    e.target.value;
+  };
   return (
     <div>
       <h1>{country[0].name.common}</h1>
       <img src={country[0].flags.png} height={100} width={120} />
-      <h1>{country[0].capital}</h1>
-      <h1>{country[0].region}</h1>
+
+      <button onClick={handleShowMore}>Show More</button>
+      {showMoreBtn && (
+        <div>
+          <h1>{country[0].capital}</h1>
+          <h1>{country[0].region}</h1>
+        </div>
+      )}
     </div>
   );
 };
