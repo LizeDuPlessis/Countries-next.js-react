837f70e2631f6042abf4ffc1cf43cf790c619f86
Lize <lizeduplessis.dev@gmail.com>
2022-03-24 08:16:33 +0000

    country list

--- pages/countries/index.js
+++ pages/countries/index.js
@@ -1,10 +1,28 @@
-const countries = () => {
+import Link from "next/link";
+export const getStaticProps = async () => {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+
+  return {
+    props: { country: data },
+  };
+};
+
+const countries = ({ country }) => {
   return (
-    <>
-      <div>
-        <h1> List of countries</h1>
-      </div>
-    </>
+    <div>
+      <h1> List of countries</h1>
+      {country.map((country) => (
+        <Link
+          href={`/countries/${country?.name.common}`}
+          key={country?.name.common}
+        >
+          <a>
+            <h3>{country?.name.common}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
   );
 };
 
